2d99cd7a8d2845fddd2a9b3a1d57144a5681d305
zigvyLong <longtt2705@gmail.com>
2021-11-01 23:00:23 +0700

    Use env url

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const header = {
     Authorization: 'Bearer'
 }
 
-const BASE_URL = '/api/'
+const BASE_URL = `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}/api/`
 
 export const get = (url: string) => axios.get(BASE_URL + url)
 export const post = (url: string, params: any) => axios.post(BASE_URL + url, params)
